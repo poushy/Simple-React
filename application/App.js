ac3510e191092fee25bae38c1e4a451ed62b9086
Poushali Mukherji <poushali.mukherji@nike.com>
2019-11-14 21:13:05 -0800

    initial

---
+++ application/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <div className="grid">
+                    <div className="item">ABC</div>
+                </div>
+                <div className="grid">
+                    <div className="item">DEF</div>
+                </div>
+            </div>
+        )
+    }
+}
